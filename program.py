12b1a81eb8a2b5c66ab91d5580e6c3e68d9f7d22
Leonardo Bertoli <leobertoli2091@gmail.com>
2024-01-19 07:33:47 -0300

    decryot method implemented

--- program.py
+++ program.py
@@ -5,17 +5,17 @@ text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
 #TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
-
-    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
-    #e.g. 
-    #plain_text = "hello"
-    #shift = 5
-    #cipher_text = "mjqqt"
-    #print output: "The encoded text is mjqqt"
-
-    ##HINT: How do you get the index of an item in a list:
-    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
-
-    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
-
+def encrypt(text, shift):
+    word = list(text)
+    for i in range(0, len(word)):
+        letter = alphabet.index(word[i])
+        if letter + shift >= len(alphabet):
+            index = (letter + shift) - len(alphabet)
+            word[i] = alphabet[index]
+        else:
+            word[i] = alphabet[letter + shift]
+    
+    print(f"The encoded text is {''.join(word)}")
+  
 #TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+encrypt(text, shift)
